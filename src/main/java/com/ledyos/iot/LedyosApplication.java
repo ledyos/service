7acd0b55adf0ae9930c89e96234ef5f7cd459cd5
ledyos <lishangjin@ledyos.com>
2016-04-11 14:57:57 +0800

    增加DockerFile

--- src/main/java/com/ledyos/iot/LedyosApplication.java
+++ src/main/java/com/ledyos/iot/LedyosApplication.java
@@ -1,12 +1,19 @@
 package com.ledyos.iot;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@RestController
 public class LedyosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LedyosApplication.class, args);
 	}
+	
+	@RequestMapping("")
+    public String hello(){
+        return "Hello! Docker!";
+    }
+	
 }

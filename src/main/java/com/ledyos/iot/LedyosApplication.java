fa73c5c5e59bd9fefe6f8e9f1db3d1225adef294
ledyos <lishangjin@ledyos.com>
2016-04-11 14:51:27 +0800

    初始化项目

---
+++ src/main/java/com/ledyos/iot/LedyosApplication.java
@@ -0,0 +1,12 @@
+package com.ledyos.iot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LedyosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LedyosApplication.class, args);
+	}
+}
